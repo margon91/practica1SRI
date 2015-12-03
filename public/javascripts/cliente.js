94a1681a5f3d3d8d41ba5efd308e8faf6ad8f410
Marta <margon@debian.debian.org>
2015-12-03 20:27:31 +0100

    servidor envia mensajes a clientes

--- public/javascripts/cliente.js
+++ public/javascripts/cliente.js
@@ -2,10 +2,13 @@ $(document).ready(function(){
 
    var socket=io.connect('localhost:3000');//Establezco un socket con el servidor en este caso como es la misma máquina localhost
 
-		socket.on('connect',function(){
-			var saludo="hola";
-			socket.emit('saludo',saludo); // A modo de ejemplo emito un saludo
-			 
-		});
+	socket.on('connect',function(){
+		var saludo="hola, te estoy saludando desde socket.io";
+		socket.emit('saludo',saludo); // A modo de ejemplo emito un saludo
+	});
+
+	socket.on('msg',function(data){
+		$('#sala').append('<p>' + data.text + '</p>');
+	});
 
 })
--- public/javascripts/servidor.js
+++ public/javascripts/servidor.js
@@ -1,10 +1,12 @@
 module.exports=function(io){
    
-  io.sockets.on ('connection',function(socket){
-  	socket.on('saludo', function(saludo){
-  		console.log(saludo);
-  		//io.socket.emit(  ); //a partir de aqui puedo empezar a emitir mensajes a los clientes
-  	});
-  })
+	io.sockets.on ('connection',function(socket){
+	  	socket.on('saludo', function(saludo){
+	  		console.log(saludo);
+	  		//var msg = "esto es una prueba";
+	  		socket.emit('msg', {text: 'esto es una prueba' });
+	  		//io.socket.emit(  ); //a partir de aqui puedo empezar a emitir mensajes a los clientes
+	  	});
+	})
 
-  }
+}
